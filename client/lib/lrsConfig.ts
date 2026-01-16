c33edf12a9e91f464acd61e91de6ce591e2ce0a9
jstevensonwgu <josh.stevenson@wgu.edu>
2026-01-16 09:38:50 -0700

    Fix syntax error in LRS auth property

--- client/lib/lrsConfig.ts
+++ client/lib/lrsConfig.ts
@@ -1,5 +1,5 @@
 export const LRS = {
   endpoint: "https://watershedlrs.com/api/organizations/27437/lrs/statements",
   version: "1.0.3",
-  auth: "base64("7bea17c52e6804:6f81e672e4b4a3",
+  auth: base64("7bea17c52e6804:6f81e672e4b4a3",
 };
