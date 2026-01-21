ed03083b3627167aab3b85ef40d3138787baaa68
jstevensonwgu <josh.stevenson@wgu.edu>
2026-01-21 12:16:53 -0700

    Update LRS endpoint to include statements path

--- client/lib/lrsConfig.ts
+++ client/lib/lrsConfig.ts
@@ -1,5 +1,5 @@
 export const LRS = {
-  endpoint: "https://xa3xvwi43b.execute-api.us-east-1.amazonaws.com",
+  endpoint: "https://xa3xvwi43b.execute-api.us-east-1.amazonaws.com/xapi/statements",
   version: "1.0.3",
   auth: "",
 };
